654b4e2fd7eebca0d20b7b8ac6fc2e91761a6b0a
RajeevKumarMahato <rajeev@teksewa.com>
2023-07-24 09:17:37 +0545

    Add project files.

---
+++ WebApplicationDemo/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata.Ecma335;
+using WebApplicationDemo.Models;
+
+namespace WebApplicationDemo.Data
+{
+    public class AppDbContext : DbContext  
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+       
+    }
+}
---
+++ WebApplicationDemo/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplicationDemo.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+    
+
+        public string Name { get; set; }
+        
+        public string DisplayOrder { get; set; }
+    }
+
+}
