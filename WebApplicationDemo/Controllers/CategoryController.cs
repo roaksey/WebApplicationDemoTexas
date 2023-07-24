b48e336940540f9626690341b2364a62ef26a6fa
RajeevKumarMahato <rajeev@teksewa.com>
2023-07-24 12:22:16 +0545

    Category List

--- WebApplicationDemo/Controllers/CategoryController.cs
+++ WebApplicationDemo/Controllers/CategoryController.cs
@@ -1,12 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApplicationDemo.Data;
 
 namespace WebApplicationDemo.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly AppDbContext _db;
+        public CategoryController(AppDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            var categories = _db.Categories.ToList();
+            return View(categories);
         }
     }
 }
