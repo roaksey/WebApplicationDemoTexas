77bbc28a8953d6cd62ed363d36a4e8a1f4be744a
RajeevKumarMahato <rajeev@teksewa.com>
2023-07-26 09:37:23 +0545

    26-07-2023 Complete Crud

--- WebApplicationDemo/Controllers/CategoryController.cs
+++ WebApplicationDemo/Controllers/CategoryController.cs
@@ -11,11 +11,18 @@ namespace WebApplicationDemo.Controllers
         {
             _db = db;
         }
+        //Read
         public IActionResult Index()
         {
             var categories = _db.Categories.ToList();
             return View(categories);
         }
+        public IActionResult Read(int id)
+        {
+            Category category = _db.Categories.FirstOrDefault(c => c.Id == id); 
+            return View(category);
+        }
+        //Create
         public IActionResult Create()
         {
             return View();
@@ -23,8 +30,59 @@ namespace WebApplicationDemo.Controllers
         [HttpPost]
         public IActionResult Create(Category model)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(model);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
         }
+
+        public IActionResult Edit(int Id)
+        {
+            Category category = _db.Categories.FirstOrDefault(x => x.Id == Id);
+            if(category is null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Update(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);  
+        }
+
+        public IActionResult Delete(int Id)
+        {
+            Category category = _db.Categories.FirstOrDefault(x => x.Id == Id);
+            if(category is null )
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult Delete(Category category)
+        {
+            if(category == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(category);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
         
     }
 }
